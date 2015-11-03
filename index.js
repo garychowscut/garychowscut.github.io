2b03bdf8ebe3b84f45108b77bf0cb4cb61ca3474
garychowscut <jializ@andrew.cme.edu>
2015-11-02 22:00:39 -0500

    yo

---
+++ index.js
@@ -0,0 +1,11 @@
+function thisIsAFunction(x){
+	return x + 5 - 10 ** 6
+}
+
+function ready(f){}
+
+$(document).ready(function (){
+$(".life-elem").click(function){
+    alert("you clicked on something!");
+  }
+})
